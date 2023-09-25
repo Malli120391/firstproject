939aab34c56c91fc7a31e3e5dea49ced61fe1f24
Malleswara Rao ch <ch.malli1203@gmail.com>
2023-09-25 12:12:13 +0530

    this is my simple python add and Hello

---
+++ Hello.py
@@ -0,0 +1,2 @@
+
+print("Hello Python how do you do!")
---
+++ addnum.py
@@ -0,0 +1,14 @@
+"""
+num1 = 1.5
+num2 = 1.5
+
+sum = num1 + num2
+#print(sum)
+print(f'The sum of {num1} and {num2} is: {sum}'.format(num1, num2, sum))"""
+
+num1 = input("Enter your number1: ")
+num2 = input("Enter your number2: ")
+
+sum = float(num1) + float(num2)
+print(f'The sum of {num1} and {num2} is: {sum}'.format(num1, num2, sum))
+
