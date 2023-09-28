146dd9f11ae853b9b017283c5c918836146644e4
Malleswara Rao ch <ch.malli1203@gmail.com>
2023-09-28 18:11:17 +0530

    some code exampls updated

---
+++ square_root.py
@@ -0,0 +1,19 @@
+
+
+"""
+num = 8
+
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))"""
+
+
+import cmath
+
+num = 1+2j
+num = eval(input('Enter a number: '))
+
+num_sqrt = cmath.sqrt(num)
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+
