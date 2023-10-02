c6787d63cda205a26fc919df24d3397c2390dba0
Malleswara Rao ch <ch.malli1203@gmail.com>
2023-10-02 20:02:50 +0530

    some code exampls updated 10/2/2023

---
+++ tuple.exercise.py
@@ -0,0 +1,24 @@
+
+
+tuple1 = (12,6,-8,'Malleswara',True, 12,6)
+#print(min(tuple1))
+#print(tuple1.count(12))
+#tuple2=(42,)
+#print(type(tuple2))
+#tuple1[0]=13
+#print(tuple1[0])
+#print(type(tuple1))
+#print(tuple1)
+#tuple2=(45,67,90)
+#print(max(tuple2))
+#tuple3=(tuple1,tuple2)
+#tuple4 = tuple1 + tuple2
+#print(tuple4)
+#print(len(tuple3))
+#print(tuple1[::2])
+
+#list1 = [1,2,3,4,5]
+#print(tuple(list1))
+
+tuple5 = (10,)*5
+print(tuple5)
