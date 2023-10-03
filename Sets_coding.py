5c24bba5ca2993d54dcce915aee86411c817abcb
Malleswara Rao ch <ch.malli1203@gmail.com>
2023-10-03 13:16:19 +0530

    some code examples updated 10/3/2023

---
+++ Sets_coding.py
@@ -0,0 +1,17 @@
+
+#set1={10,56,89,90, 'Malleswara', True,10,1}
+set1={10,56,89,90, 'Malleswara', True}
+#set2 = set()
+#print(type(set1))
+#set1.add(99)
+#set1.remove(10)
+#print(set1)
+#set1.discard(89)
+#print(set1)
+#print(len(set1))
+#print(type(set2))
+#print(set1.pop())
+#print(set1)
+
+set1.add((13,14,15))
+print(set1)
---
+++ coding_exercise9.py
@@ -0,0 +1,27 @@
+
+"""num = float(input("Enter your Number: "))
+if num > 0:
+    print("Positive Number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative Number")"""
+
+"""num = float(input("Enter a number: "))
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
+else:
+   print("Negative number")"""
+
+num = int(input('Enter your number: '))
+if (num % 2) == 0:
+    print("{0} is Even".format(num))
+else:
+    print("{0} is Odd".format(num))
+
+
+
+
