20fa68fbb425168fb82bd2e36a41306e5a3dfa48
Malleswara Rao ch <ch.malli1203@gmail.com>
2023-10-25 16:15:17 +0530

    some code examples updated 10/25/2023

---
+++ break_continu_pass.py
@@ -0,0 +1,52 @@
+"""count = 1
+while count<=10:
+    print(count)
+    count += 1
+    if count == 7:
+        break
+    print("Hey")
+print("out from loop")"""
+
+"""list1 = ["hi", "hello", "welcome"]
+names = ["om", "nama", "shivaya"]
+for item in list1:
+    for name in names:
+        print(item,name)
+        if item=="hello" and name=="nama":
+            break
+    print("out from inner loop")
+print("out from outer loop")"""
+
+"""count = 1
+while count<=10:
+    print(count)
+    count += 1
+    if count == 7:
+        continue
+    print("Hey")
+print("out from loop")"""
+
+"""for i in range(1,11):
+    if i==7:
+        continue
+    else:
+        print(i)"""
+"""count = 1
+while count<=10:
+    print(count)
+    count += 1
+    if count == 7:
+        pass
+    print("Hey")
+print("out from loop")"""
+
+"""for i in range(1,11):
+    pass"""
+
+def func1():
+    pass
+    #print("Hello")
+#func1()
+
+
+
