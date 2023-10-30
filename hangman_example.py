8f189a89b9c40b69bffdebd2c1b410d009ffa023
Malleswara Rao ch <ch.malli1203@gmail.com>
2023-10-30 10:29:09 +0530

    some code examples updated 10/30/2023

---
+++ hangman_example.py
@@ -0,0 +1,30 @@
+import random
+import hangman_stages
+import word_file
+#word_list = ["apple", "beautiful", "potato"]
+lives=6
+chosen_word=random.choice(word_file.words)
+print(chosen_word)
+display=[]
+for i in range(len(chosen_word)):
+    display += '_'
+print(display)
+game_over=False
+while not game_over:
+    guessed_letter = input("Guess a latter:").lower()
+    for position in range(len(chosen_word)):
+        letter=chosen_word[position]
+        if letter==guessed_letter:
+           display[position]==guessed_letter
+    print(display)
+    if guessed_letter not in chosen_word:
+        lives -= 1
+        if letter == 0:
+            game_over = True
+            print("You lose!")
+    if '_' not in display:
+        game_over = True
+        print("You Win!")
+    print(hangman_stages.stages[lives])
+
+
---
+++ word_file.py
@@ -0,0 +1,9 @@
+words=[
+"chennu",
+"apple",
+"beautiful",
+"orange",
+"malleswararao",
+"data engineer",
+"aeroplane"
+]
