26d5d04c3f3b38a7e8d61088a7a364479e2b9975
Malleswara Rao ch <ch.malli1203@gmail.com>
2023-11-25 13:45:10 +0530

    some code examples updated 11/25/2023

---
+++ dictionaries_ex.py
@@ -0,0 +1,37 @@
+"""phone_no={
+    'Ram':1234,
+    'Jathin': 3456,
+    'Thukaram' : 8976
+}"""
+phone_no=dict({
+    'Malli':1234,
+    'Jathin': 3456,
+    'Thukaram' : 8976
+})
+print(phone_no)
+#phone_no2=phone_no.copy()
+#print(phone_no2)
+print(len(phone_no))
+#phone_no['Malli']=9999
+#phone_no['NagaRaju']={111,222,333}
+#phone_no['Jathin']={'Jathin_Home': 555, 'Jathin_work': 999}
+#print(phone_no['Jathin']['Jathin_Home'])
+#print(phone_no.get('jathin'))
+#print(phone_no.pop('Thukaram'))
+#print(phone_no.popitem())
+#del phone_no['Malli']
+#print(phone_no.clear())
+#print(phone_no.keys())
+#print(phone_no.values())
+#print(phone_no.items())
+#print(phone_no)
+#for i in phone_no.items():
+    #print(i)
+    #print(phone_no[i])
+
+"""data={
+    1:'Mall1',
+    2:'Mohan',
+    0:'Adi'
+}
+print(data[1])"""
