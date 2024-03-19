5514bf156b1dfb1edbfe53666f59c46e0dc2e52e
Malleswara Rao ch <ch.malli1203@gmail.com>
2024-03-19 18:09:55 +0530

    this is some code 03/19/2024

---
+++ quiz_database.py
@@ -0,0 +1,15 @@
+question_bank = [
+    {"text": "The ability of one class to acquire methods and attributes from another class is called___.",
+     "answer": "A"},
+    {"text": "which of the following is a type of inheritance?", "answer": "D"},
+    {"text": "what type of inheritance has multiple subclasses to a single superclass?", "answer": "C"},
+    {"text": "Whta is the depth of multilevel inheritance in Python?", "answer": "C"},
+    {"text": "what does MRO stand for?", "answer": "B"}
+]
+options = [["A. Inheritance", "B. Abstraction", "C. Polymorphism", "D. Objects"],
+           ["A. Single", "B. Double", "C. Multiple", "D. both A and C"],
+           ["A. Multiple Inheritance", "B. Multilevel Inheritance", "C. Hierarchical Inheritance", "D. None of these"],
+           ["A. Two level", "B. Three level", "C. Any level", "D. None of these"],
+           ["A. Method Recursive Object", "B. Method Resolution Order", "C. Main Resolution Order",
+            "D. Method Resolution Object"]
+           ]
